af657daecc2c14e70403ba64f25bbafc0dbb5344
Jon Pretty <jon.pretty@propensive.com>
2014-04-13 21:23:54 +0100

    Added missing build files

---
+++ project/project.scala
@@ -0,0 +1,17 @@
+object project extends ProjectSettings {
+  def scalaVersion = "2.10.4"
+  def version = "0.9.0"
+  def name = "uri"
+  def description = "Rapture URI provides idiomatic and extensible support for working with URIs and URLs in Scala."
+  
+  def dependencies = Seq(
+    "core" -> "0.9.0"
+  )
+  
+  def thirdPartyDependencies = Nil
+
+  def imports = Seq(
+    "rapture.core._",
+    "rapture.test._"
+  )
+}
