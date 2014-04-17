7017b38128d88abe237fc86183c21cd42bcf320f
Jon Pretty <jon.pretty@propensive.com>
2014-04-17 14:42:59 +0100

    Switched to Scala 2.11 final

--- project/project.scala
+++ project/project.scala
@@ -1,5 +1,5 @@
 object project extends ProjectSettings {
-  def scalaVersion = "2.11.0-RC4"
+  def scalaVersion = "2.11.0"
   def version = "0.9.1"
   def name = "uri"
   def description = "Rapture URI provides idiomatic and extensible support for working with URIs and URLs in Scala."
