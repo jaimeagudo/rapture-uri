2131a22ba7db9c26afb591bfa02d6925b47b7ff5
Jon Pretty <jon.pretty@propensive.com>
2014-05-11 12:38:40 +0100

    Renamed Rts to Mode

--- src/nav.scala
+++ src/nav.scala
@@ -24,23 +24,23 @@ import rapture.core._
 import java.io._
 import java.net._
 
-trait UriMethods extends RtsGroup
+trait UriMethods extends ModeGroup
 
 trait Navigable[UrlType] {
   
   private implicit val errorHandler = raw
   
-  def children(url: UrlType)(implicit rts: Rts[UriMethods]): rts.Wrap[List[UrlType], Exception]
+  def children(url: UrlType)(implicit mode: Mode[UriMethods]): mode.Wrap[List[UrlType], Exception]
   
   /** Returns false if the filesystem object represented by this FileUrl is a file, and true if
     * it is a directory. */
-  def isDirectory(url: UrlType)(implicit rts: Rts[UriMethods]): rts.Wrap[Boolean, Exception]
+  def isDirectory(url: UrlType)(implicit mode: Mode[UriMethods]): mode.Wrap[Boolean, Exception]
   
   /** If this represents a directory, returns an iterator over all its descendants,
     * otherwise returns the empty iterator. */
-  def descendants(url: UrlType)(implicit rts: Rts[UriMethods]):
-      rts.Wrap[Iterator[UrlType], Exception] =
-    rts wrap {
+  def descendants(url: UrlType)(implicit mode: Mode[UriMethods]):
+      mode.Wrap[Iterator[UrlType], Exception] =
+    mode wrap {
       children(url).iterator.flatMap { c =>
         if(isDirectory(c)) Iterator(c) ++ descendants(c)
         else Iterator(c)
@@ -53,19 +53,19 @@ class NavigableExtras[UrlType: Navigable](url: UrlType) {
   protected implicit val errorHandler = raw
   
   /** Return a list of children of this URL */
-  def children(implicit rts: Rts[UriMethods]) =
-    rts wrap ?[Navigable[UrlType]].children(url)
+  def children(implicit mode: Mode[UriMethods]) =
+    mode wrap ?[Navigable[UrlType]].children(url)
   
   /** Return true if this URL node is a directory (i.e. it can contain other URLs). */
-  def isDirectory(implicit rts: Rts[UriMethods]): rts.Wrap[Boolean, Exception] =
-    rts wrap ?[Navigable[UrlType]].isDirectory(url)(raw)
+  def isDirectory(implicit mode: Mode[UriMethods]): mode.Wrap[Boolean, Exception] =
+    mode wrap ?[Navigable[UrlType]].isDirectory(url)(raw)
 
   /** Return an iterator of all descendants of this URL. */
-  def descendants(implicit rts: Rts[UriMethods]): rts.Wrap[Iterator[UrlType], Exception] =
-    rts wrap ?[Navigable[UrlType]].descendants(url)(raw)
+  def descendants(implicit mode: Mode[UriMethods]): mode.Wrap[Iterator[UrlType], Exception] =
+    mode wrap ?[Navigable[UrlType]].descendants(url)(raw)
 
-  def walkFilter(cond: UrlType => Boolean)(implicit rts: Rts[UriMethods]):
-      rts.Wrap[List[UrlType], Exception] = rts wrap {
+  def walkFilter(cond: UrlType => Boolean)(implicit mode: Mode[UriMethods]):
+      mode.Wrap[List[UrlType], Exception] = mode wrap {
     children(raw) filter cond flatMap { f =>
       new NavigableExtras(f).walkFilter(cond)
     }
